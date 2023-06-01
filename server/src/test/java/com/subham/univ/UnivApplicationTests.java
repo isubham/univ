407fc662e55e29a12d06c88063c6bb2906084b3d
isubham <subhamkumarchandrawansi@gmail.com>
2023-06-01 22:05:07 +0530

    project setup

---
+++ server/src/test/java/com/subham/univ/UnivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subham.univ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
